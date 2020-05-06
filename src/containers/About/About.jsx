e79dff9134dbdcfee3ee053546572eafd6f771a4
JaysonD <jaduff20@gmail.com>
2020-05-06 17:59:21 -0400

    images imported correctly, mvp in place

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./About.css";
+import face from "../../imgs/myface.png"
+import resume from "../../imgs/JaysonDuffResume2020 (2).pdf"
 
 const About = () => {
     return (
@@ -9,7 +11,7 @@ const About = () => {
                     <article class="block rounded">
                         <h1 id="about-me" class="block-header">About Me</h1>
                         <hr />
-                        <img src="assets/imgs/myface.png" alt="" id="about-image" />
+                        <img src={face} alt="" id="about-image" />
                         <p>
                             Coming from a background of art and design as well as commercial insurance,
                             I currently merge my diverse skillset in data and design as a software developer. My primary interests are in web development
@@ -20,7 +22,7 @@ const About = () => {
                             When not at work I enjoy drawing, tinkering with single board computers, and listening to audiobooks (sometimes all at once). If you prefer, feel free to contact me directly at: <br /> <strong>Email: </strong>jaduff20@gmail.com <br /> <strong>Phone: </strong>404.375.6989
                         </p>
                     </article>
-                    <a id="resume" class="btn btn-dark" href="assets/imgs/JaysonDuffResume2020 (2).pdf" download>Resume</a>
+                    <a id="resume" class="btn btn-dark" href={resume} download>Resume</a>
                     <a id="linkedin" type="button" class="btn btn-dark" href="https://www.linkedin.com/in/jaysonduff/" target="_blank">LinkedIn</a>
                     <a id="github" type="button" class="btn btn-dark" href="https://github.com/fmiusov" target="_blank">GitHub</a>
                 </div>
